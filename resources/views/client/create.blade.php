e25cc6e2abf377212ccd3e4d32f4e2aa2d64883b
EEDS5 <duransandoval777@gmail.com>
2023-09-19 00:20:52 -0400

    Guardando cambios locales

---
+++ resources/views/client/create.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Crear Cliente</title>
+</head>
+<body>
+    <h1>Crear Cliente</h1>
+    <form method="POST" action="{{ route('client.store') }}">
+        @csrf
+        <label for="fullName">Full Name:</label>
+        <input type="text" name="fullName" required><br><br>
+        <label for="passwordSalt">Password Salt (BLOB):</label>
+        <input type="text" name="passwordSalt"><br><br>
+        <label for="passwordHash">Password Hash (BLOB):</label>
+        <input type="text" name="passwordHash"><br><br>
+        <label for="phone">Phone:</label>
+        <input type="text" name="phone"><br><br>
+        <label for="email">Email:</label>
+        <input type="email" name="email" required><br><br>
+        <input type="submit" value="Crear Cliente">
+    </form>
+</body>
+</html>
