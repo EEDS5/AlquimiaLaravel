0804c8ce26e4984273cc270cabdabd3eb1ab3b78
matiasricosuave <matiascrack0@gmail.com>
2023-09-19 05:36:46 +0200

    Rutas y vistas

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,14 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\SemesterController;
+use App\Http\Controllers\ServingTurnController;
+use App\Http\Controllers\ClientController;
+use App\Http\Controllers\ClientTokenController;
+use App\Http\Controllers\ReservationController;
+use App\Http\Controllers\ClientNoteController;
+use App\Http\Controllers\PlateController;
+use App\Http\Controllers\PlateImageController;
+use App\Http\Controllers\MenuController;
 
 
 /*
@@ -18,5 +26,62 @@ use App\Http\Controllers\SemesterController;
 Route::get('/', function () {
     return view('welcome');
 });
+// Rutas para SemesterController
 
-Route::resource('semester', SemesterController::class);
+Route::resource('semesters', SemesterController::class);
+
+
+
+
+// Rutas para ServingTurnController
+
+Route::resource('serving-turns', ServingTurnController::class);
+
+
+
+
+// Rutas para ClientController
+
+Route::resource('client', ClientController::class);
+
+
+
+
+// Rutas para ClientTokenController
+
+Route::resource('client-tokens', ClientTokenController::class);
+
+
+
+
+// Rutas para ReservationController
+
+Route::resource('reservations', ReservationController::class);
+
+
+
+
+// Rutas para ClientNoteController
+
+Route::resource('client-notes', ClientNoteController::class);
+
+
+
+
+// Rutas para PlateController
+
+Route::resource('plates', PlateController::class);
+
+
+
+
+// Rutas para PlateImageController
+
+Route::resource('plate-images', PlateImageController::class);
+
+
+
+
+// Rutas para MenuController
+
+Route::resource('menus', MenuController::class);
